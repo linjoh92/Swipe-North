d3d1c69470147f6bb8ee66c3591a51a651fbe64e
petksks <peter.sedberg@gmail.com>
2023-03-29 14:46:08 +0200

    added swipe function

--- app/page.js
+++ app/page.js
@@ -1,16 +1,29 @@
+'use client';
+import React, { useState } from 'react'
 import LikeBar from './likeBar'
 import styles from './page.module.css'
 import SwipeCard from './swipeCardLayout'
 import jobInfo from './jobAPI'
 
 export default function Home() {
+  const [currentJobIndex, setCurrentJobIndex] = useState(0)
+  const currentJob = jobInfo[currentJobIndex]
+
+  const handleSwipe = (direction) => {
+    setCurrentJobIndex((prevIndex) => {
+      if (direction === 'right') {
+        return (prevIndex + 1) % jobInfo.length;
+      } else {
+        return prevIndex === 0 ? jobInfo.length - 1 : prevIndex - 1;
+      }
+    });
+  };
+  
   return (
     <section className={styles.homeContainer}>
       <main className={styles.main}>
         <div className={styles.swipeJobCard}>
-          {jobInfo.map((job) => (
-            <SwipeCard key={job.id} {...job} />
-          ))}
+          {currentJob && <SwipeCard key={currentJob.id} {...currentJob} onSwipe={handleSwipe} />}
         </div>
       </main>
       <LikeBar />
--- app/swipeCardLayout.js
+++ app/swipeCardLayout.js
@@ -1,56 +1,56 @@
-'use client'
-import styles from './page.module.css'
-import { useState } from 'react'
+import React from 'react'
+import TinderCard from 'react-tinder-card'
+import { motion } from 'framer-motion'
 import { MdInfoOutline } from 'react-icons/md'
 import { BsChevronDown } from 'react-icons/bs'
-import { motion } from 'framer-motion'
 import { useLanguage } from './language'
 import Modal from './modal.jobInfo'
 import { atom, useAtom } from 'jotai'
-import LikeButton from './likeButton'
 
+// Define the likedJobsAtom to manage liked job state
 const likedJobsAtom = atom([])
 
 export default function SwipeCard(props) {
-  const { id, imgUrl, companyName, jobTitle, shortInfo, days, type, link } =
-    props
-  const [showModal, setShowModal] = useState(false)
+    // Destructure props to get job information
+  const { id, imgUrl, companyName, jobTitle, shortInfo, days, type, link } = props
   const { text } = useLanguage('swe')
+  const [showModal, setShowModal] = React.useState(false)
+    // Use the likedJobsAtom to manage liked jobs state
   const [likedJobs, setLikedJobs] = useAtom(likedJobsAtom)
 
-  const handleClick = (e) => {
-    e.preventDefault()
+    // Function to handle swiping the card
+  const swiped = (direction) => {
+    console.log('Swiped', direction)
+    props.onSwipe(direction)
+  }  
+
+    // Function to handle when the card is out of the frame
+  const outOfFrame = (job) => {
+    console.log(job.title, 'left the screen!')
+  }
+  // Function to handle opening the job information modal
+  const handleClick = () => {
     setShowModal(true)
   }
 
-  const isJobLiked = likedJobs.some((job) => job.id === id)
+  // Check if the job is liked
+  const isJobLiked = likedJobs.includes(id)
+
+    // Function to handle liking and unliking jobs
   const handleLike = () => {
-    const newJob = {
-      id,
-      imgUrl,
-      companyName,
-      jobTitle,
-      shortInfo,
-      days,
-      type,
-      link,
-    }
-    if (isJobLiked) {
-      const newLikedJobs = likedJobs.filter((job) => job.id !== id)
-      setLikedJobs(newLikedJobs)
-      localStorage.setItem('savedJobs', JSON.stringify(newLikedJobs))
-    } else {
-      setLikedJobs([...likedJobs, newJob])
-      localStorage.setItem('savedJobs', JSON.stringify([...likedJobs, newJob]))
-    }
+    const newLikedJobs = isJobLiked
+      ? likedJobs.filter((jobId) => jobId !== id)
+      : [...likedJobs, id]
+    setLikedJobs(newLikedJobs)
+    localStorage.setItem('savedJobs', JSON.stringify(newLikedJobs))
   }
 
   return (
-    <div className={styles.swipeContainer}>
+    <div className="swipe-card-container">
       {showModal && (
         <>
           <motion.div
-            className={styles.overlay}
+            className="overlay"
             style={{ backgroundImage: `url(${imgUrl})` }}
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
@@ -71,15 +71,24 @@ export default function SwipeCard(props) {
           />
         </>
       )}
-      <div className={styles.swipeCardConatiner}>
+      <TinderCard
+        className="swipe-card"
+        onSwipe={(dir) => swiped(dir)}
+        onCardLeftScreen={() => outOfFrame(props)}
+      >
         <div
-          className={styles.swipeCard}
+          className="card"
           style={{ backgroundImage: `url(${imgUrl})` }}
         >
-          <LikeButton isLiked={isJobLiked} onClick={handleLike} />
+          <button
+            className={`heart-button${isJobLiked ? ' liked' : ''}`}
+            onClick={handleLike}
+          >
+            <span className="heart-icon"></span>
+          </button>
 
           <motion.div
-            className={styles.uppArrow}
+            className="upp-arrow"
             animate={{ y: -7 }}
             transition={{ type: 'spring', repeat: Infinity, repeatDelay: 2 }}
           >
@@ -93,7 +102,8 @@ export default function SwipeCard(props) {
             />
           </motion.div>
 
-          <div className={styles.infoConatiner} onClick={handleClick}>
+              {/* job info container */}
+          <div className="info-container" onClick={handleClick}>
             <h2>
               {companyName}
               <MdInfoOutline />
@@ -101,7 +111,7 @@ export default function SwipeCard(props) {
             <p>{jobTitle}</p>
           </div>
         </div>
-      </div>
+      </TinderCard>
     </div>
   )
 }
